58749090172d25bcbb007c424a41e35c5d9c15ba
devopscomputer <pauloti.engenharia@gmail.com>
2025-08-03 03:44:50 -0300

    (docs): Organizando a Documentaçao de todas as fases do projeto robusto

--- backend-cleaning/src/search/search.module.ts
+++ backend-cleaning/src/search/search.module.ts
@@ -1,14 +1,17 @@
+// src/search/search.module.ts
 import { Module } from '@nestjs/common';
 import { SearchService } from './search.service';
 import { SearchController } from './search.controller';
 import { ProvidersModule } from '../providers/providers.module'; // Importa o módulo de provedores
 import { ServicesModule } from '../services/services.module';   // Importa o módulo de tipos de serviço
+import { ProviderServicesModule } from '../provider-services/provider-services.module'; // NOVO: Importa o módulo de serviços de provedores
 import { OffersModule } from '../offers/offers.module'; // Importa o módulo de ofertas
 
 @Module({
   imports: [
     ProvidersModule,
     ServicesModule,
+    ProviderServicesModule, // NOVO: Adicione ProviderServicesModule aqui
     OffersModule, // Se houver um módulo de ofertas e você quiser buscar ofertas
   ],
   controllers: [SearchController],
