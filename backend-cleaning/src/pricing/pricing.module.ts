03f1b748eb101467527b4707a10a69f3ded1c55a
devopscomputer <pauloti.engenharia@gmail.com>
2025-10-04 11:13:34 -0300

    chore: update backend-cleaning/src/pricing/pricing.module.ts

--- backend-cleaning/src/pricing/pricing.module.ts
+++ backend-cleaning/src/pricing/pricing.module.ts
@@ -3,13 +3,13 @@ import { Module, forwardRef } from '@nestjs/common';
 import { PricingService } from './pricing.service';
 import { PricingController } from './pricing.controller';
 import { PrismaModule } from '../prisma/prisma.module';
-import { GeocodingModule } from '../geocoding/geocoding.module';
+import { GeocodingModule } from '../geocoding/geocoding.module';\nimport { CacheModule } from '../cache/cache.module';
 import { BookingsModule } from '../bookings/bookings.module';
 
 @Module({
   imports: [
     PrismaModule,
-    GeocodingModule,
+    GeocodingModule,\n    CacheModule,
     forwardRef(() => BookingsModule),
   ],
   controllers: [PricingController],
