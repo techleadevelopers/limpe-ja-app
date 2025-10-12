3986eddd51212e760bbba98d50810b38c0fca29b
devopscomputer <pauloti.engenharia@gmail.com>
2025-10-12 08:03:24 -0300

    chore: update app/(auth)/register-options.tsx

--- app/(auth)/register-options.tsx
+++ app/(auth)/register-options.tsx
@@ -199,7 +199,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#F7F8FC',
+        backgroundColor: 'transparent',
         padding: 25,
     },
     logoContainer: {
