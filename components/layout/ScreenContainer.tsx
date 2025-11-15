b84c67001d2be61b068c29934bfa17fe0b2b8d98
devopscomputer <pauloti.engenharia@gmail.com>
2025-11-15 12:50:56 -0300

    chore: update components/layout/ScreenContainer.tsx

--- components/layout/ScreenContainer.tsx
+++ components/layout/ScreenContainer.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import { View, ViewProps, StyleSheet } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { useDevice } from '@/utils/responsive';
 
 export default function ScreenContainer({ style, children, ...rest }: ViewProps) {
   const { isLargePhone } = useDevice();
+  const insets = useSafeAreaInsets();
+
   return (
-    <View style={[styles.root, isLargePhone && styles.maxW, style]} {...rest}>
+    <View
+      style={[
+        styles.root,
+        {
+          paddingTop: 5, // ou -12, etc.
+    paddingBottom: -12,
+        },
+        isLargePhone && styles.maxW,
+        style,
+      ]}
+      {...rest}
+    >
       {children}
     </View>
   );
@@ -14,6 +28,6 @@ export default function ScreenContainer({ style, children, ...rest }: ViewProps)
 const styles = StyleSheet.create({
   root: { flex: 1 },
   // só aplica no Pro Max; 12 Pro fica igual
-  maxW: { alignSelf: 'center', width: '100%', maxWidth: 820, paddingHorizontal: 20 },
+  maxW: { alignSelf: 'center', width: '100%', maxWidth: 820, paddingHorizontal: 10 },
 });
 
