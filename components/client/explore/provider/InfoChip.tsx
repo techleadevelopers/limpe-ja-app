679e236188995eb13bde38f0dcc7e29dd9ad653c
devopscomputer <pauloti.engenharia@gmail.com>
2025-11-28 12:25:30 -0300

    chore: update components/client/explore/provider/InfoChip.tsx

--- components/client/explore/provider/InfoChip.tsx
+++ components/client/explore/provider/InfoChip.tsx
@@ -2,7 +2,7 @@
 import { Ionicons } from '@expo/vector-icons';
 import React from 'react';
 import { Text, View } from 'react-native';
-import { styles } from '../../../../app/(client)/explore/styles/providerStyles';
+import { styles } from '../../../../styles/providerStyles';
 
 interface InfoChipProps {
   iconName: keyof typeof Ionicons.glyphMap;
