c6a7433b124fe69ba4593b7c93bd6a84e6db2c95
devopscomputer <pauloti.engenharia@gmail.com>
2025-12-20 11:59:09 -0300

    chore: update components/client/booking/success/ImmediateActionButtons.tsx

--- components/client/booking/success/ImmediateActionButtons.tsx
+++ components/client/booking/success/ImmediateActionButtons.tsx
@@ -1,4 +1,4 @@
-// LimpeJaApp/app/(client)/bookings/components/success/ImmediateActionButtons.tsx
+// LimpeJaApp/app/client/bookings/components/success/ImmediateActionButtons.tsx
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, Platform, Dimensions } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
