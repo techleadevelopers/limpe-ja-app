48e4f0c7c0d05d1ed21eb1f075b7222a31f622a6
devopscomputer <pauloti.engenharia@gmail.com>
2025-12-20 11:59:57 -0300

    chore: update tmp-script.js

--- tmp-script.js
+++
@@ -1,7 +0,0 @@
-﻿const fs = require("fs");
-const path = require("path");
-
-const filePath = path.join(process.cwd(), 'components/client/booking/success/BookingSummaryCard.tsx');
-let source = fs.readFileSync(filePath, 'utf8');
-
-const oldImportBlock = "import { BlurView } from 'expo-blur';\nimport { LinearGradient } from 'expo-linear-gradient';\nimport React from 'react';\nimport { Animated, Platform, StyleSheet, View, Text, Dimensions } from 'react-native'; // ✅ NOVO: AccessibilityInfo para A11y\n\n";
