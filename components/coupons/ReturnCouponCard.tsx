9cf8fff58e365b74295374dfcfa11ce70d6401e8
devopscomputer <pauloti.engenharia@gmail.com>
2025-12-25 16:53:03 -0300

    chore: update components/coupons/ReturnCouponCard.tsx

--- components/coupons/ReturnCouponCard.tsx
+++ components/coupons/ReturnCouponCard.tsx
@@ -141,7 +141,7 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     marginTop: -10,
     marginBottom: -5,
-    ...AppShadows.medium,
+    ,
   },
   gradientBackground: {
     padding: 20,
