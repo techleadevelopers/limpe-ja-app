2a35b1910bdc33e59f93888a560e600655e5f850
devopscomputer <pauloti.engenharia@gmail.com>
2025-12-25 16:52:52 -0300

    chore: update components/client/explore/provider/ReviewCard.tsx

--- components/client/explore/provider/ReviewCard.tsx
+++ components/client/explore/provider/ReviewCard.tsx
@@ -63,7 +63,7 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 1 },
     shadowOpacity: 0.05,
     shadowRadius: 2,
-    elevation: 2,
+    elevation: 0,
   },
   // CORREÇÃO 3: Header limpo
   reviewHeader: {
