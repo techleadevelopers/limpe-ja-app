65ef3f24325d7120ce48ee690571f9ed4a60662f
devopscomputer <pauloti.engenharia@gmail.com>
2026-01-02 21:25:26 -0300

    chore: update e2e/environment.js

--- e2e/environment.js
+++ e2e/environment.js
@@ -1,13 +1,13 @@
 // e2e/environment.js
+const { DetoxCircusEnvironment } = require('detox/runners/jest');
 const {
-  DetoxCircusEnvironment,
   SpecReporter,
   WorkerAssignReporter,
-} = require('detox/runners/jest-circus');
+} = require('detox/runners/jest/testEnvironment/listeners');
 
 class CustomDetoxEnvironment extends DetoxCircusEnvironment {
-  constructor(config) {
-    super(config);
+  constructor(config, context) {
+    super(config, context);
 
     // Este reporter é útil para ver o progresso dos testes em tempo real
     this.registerListeners({
