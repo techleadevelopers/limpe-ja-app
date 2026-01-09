da02951244b1b4c0bd782c2d44e170d57ff765f6
devopscomputer <pauloti.engenharia@gmail.com>
2026-01-09 09:47:33 -0300

    chore: update components/client/explore/home/NewHeader.tsx

--- components/client/explore/home/NewHeader.tsx
+++ components/client/explore/home/NewHeader.tsx
@@ -251,7 +251,7 @@ const styles = StyleSheet.create({
     borderBottomStartRadius: 40,
     borderTopEndRadius: 40,
     borderTopStartRadius: 40,
-    marginBottom: Platform.OS === 'android' ? 5 : -8,
+    marginBottom: Platform.OS === 'android' ? 0 : -8,
 
     flexDirection: 'row',
     justifyContent: 'space-between',
