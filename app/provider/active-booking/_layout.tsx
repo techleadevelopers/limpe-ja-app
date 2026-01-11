40f1c09c4c5523e25bf565117f02305483405ae0
devopscomputer <pauloti.engenharia@gmail.com>
2026-01-10 23:10:14 -0300

    chore: update app/provider/active-booking/_layout.tsx

--- app/provider/active-booking/_layout.tsx
+++ app/provider/active-booking/_layout.tsx
@@ -4,7 +4,7 @@ import { Stack } from 'expo-router';
 export default function ProviderActiveBookingStackLayout() {
   return (
     <Stack screenOptions={{ headerShown: true, headerShadowVisible: false, headerBackTitleVisible: false }}>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="[bookingId]" options={{ headerShown: false }} />
     </Stack>
   );
 }
