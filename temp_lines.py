7332aeb7e93f830da12e6ffc5bac4c9cd9416fc8
devopscomputer <pauloti.engenharia@gmail.com>
2026-01-15 16:23:25 -0300

    chore: update temp_lines.py

--- temp_lines.py
+++
@@ -1,8 +0,0 @@
-﻿import pathlib
-lines = pathlib.Path('styles/providerStyles.ts').read_text(encoding='utf-8').splitlines()
-for idx,line in enumerate(lines):
-    if 'newProviderBadgeContainer' in line:
-        for j in range(idx, idx+30):
-            print(j+1, lines[j])
-        break
-
