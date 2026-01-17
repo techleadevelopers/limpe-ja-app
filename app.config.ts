7a06aa0df58ee26ad1d143c7bb7478c3cec2e307
devopscomputer <pauloti.engenharia@gmail.com>
2026-01-17 13:58:42 -0300

    chore: update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -10,7 +10,7 @@ export default ({ config }: { config: ExpoConfig }): ExpoConfig => {
 
     name: 'LimpeJá',
     slug: 'limpeja',
-    version: '1.0.31',
+    version: '1.0.32',
     orientation: 'portrait',
     icon: './assets/images/icon.png',
     scheme: 'cleaning',
@@ -28,7 +28,7 @@ export default ({ config }: { config: ExpoConfig }): ExpoConfig => {
       supportsTablet: false,
       bundleIdentifier: 'com.techleadevelopers.limpeja',
       googleServicesFile: './GoogleService-Info.plist',
-      buildNumber: '35',
+      buildNumber: '36',
       infoPlist: {
         ITSAppUsesNonExemptEncryption: false,
         NSCameraUsageDescription:
@@ -45,7 +45,7 @@ export default ({ config }: { config: ExpoConfig }): ExpoConfig => {
     },
 
     android: {
-      versionCode: 19,
+      versionCode: 20,
       googleServicesFile: './google-services.json',
       adaptiveIcon: {
         foregroundImage: './assets/images/adaptive-icon.png',
