5470da1cffb28a731c24bff5ead19678ebfb4cfd
devopscomputer <pauloti.engenharia@gmail.com>
2026-01-20 17:48:49 -0300

    chore: update backend-cleaning/src/app.module.ts

--- backend-cleaning/src/app.module.ts
+++ backend-cleaning/src/app.module.ts
@@ -66,8 +66,10 @@ import { DisputeModule } from './disputes/dispute.module';
 // NOVOS recursos
 import { LocksModule } from './common/locks/locks.module';
 import { MetricsModule } from './metrics/metrics.module';
+import { ObservabilityModule } from './observability/observability.module';
 import { SupportModule } from './support/support.module';
 import { MetaModule } from './meta/meta.module';
+import { TelemetryModule } from './telemetry/telemetry.module';
 
 // Admin
 import { AdminModule } from './admin/admin.module';
@@ -150,6 +152,8 @@ import { ExpireBookingsJob } from './worker/expire-bookings.job';
     AdminModule,
     LocksModule,
     MetricsModule,
+    ObservabilityModule,
+    TelemetryModule,
     SupportModule,
     MetaModule,
     AnalyticsModule,
