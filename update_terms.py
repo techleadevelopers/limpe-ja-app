b5a1eab0a2a79b8da6e6f482c74bc221abaf301c
devopscomputer <pauloti.engenharia@gmail.com>
2026-01-24 12:25:43 -0300

    chore: update update_terms.py

--- update_terms.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-path = Path('app/provider/profile/index.tsx')
-data = path.read_text()
-before, rest = data.split('const handleAcceptTerms = async () =,1)
-body, after = rest.split('const handleLogout',1)
