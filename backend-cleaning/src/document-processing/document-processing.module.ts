6b89b807bfd6107e6479e4c5898ccd7bf9c75f8a
devopscomputer <pauloti.engenharia@gmail.com>
2026-01-24 12:18:16 -0300

    chore: update backend-cleaning/src/document-processing/document-processing.module.ts

--- backend-cleaning/src/document-processing/document-processing.module.ts
+++ backend-cleaning/src/document-processing/document-processing.module.ts
@@ -1,11 +1,11 @@
 // src/document-processing/document-processing.module.ts
 import { Module } from '@nestjs/common';
 import { UploadModule } from '../upload/upload.module';
-import { DocumentProcessingService } from './document-processing.service'; // Importe o serviço do diretório correto
+import { DocumentProcessingService } from './document-processing.service';
 
 @Module({
   imports: [UploadModule],
   providers: [DocumentProcessingService],
-  exports: [DocumentProcessingService], // Exporta o serviço para que outros módulos possam injetá-lo
+  exports: [DocumentProcessingService],
 })
 export class DocumentProcessingModule {}
