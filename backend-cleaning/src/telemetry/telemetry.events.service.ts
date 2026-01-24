7ec7592d89e66526bf806c8199b59028ab5b037b
devopscomputer <pauloti.engenharia@gmail.com>
2026-01-24 12:19:39 -0300

    chore: update backend-cleaning/src/telemetry/telemetry.events.service.ts

--- backend-cleaning/src/telemetry/telemetry.events.service.ts
+++ backend-cleaning/src/telemetry/telemetry.events.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { EventEmitter } from 'events';
-import { TelemetryAnomalyPayload, TELEMETRY_ANOMALY_EVENT } from './telemetry.types';
+import {
+  TelemetryAnomalyPayload,
+  TELEMETRY_LATENCY_SPIKE_EVENT,
+  TELEMETRY_ANOMALY_EVENT,
+  TelemetryLatencySpikePayload,
+} from './telemetry.types';
 
 @Injectable()
 export class TelemetryEventsService extends EventEmitter {
@@ -11,5 +16,9 @@ export class TelemetryEventsService extends EventEmitter {
 
   publishAnomaly(payload: TelemetryAnomalyPayload): void {
     this.emit(TELEMETRY_ANOMALY_EVENT, payload);
+  }
+
+  publishLatencySpike(payload: TelemetryLatencySpikePayload): void {
+    this.emit(TELEMETRY_LATENCY_SPIKE_EVENT, payload);
   }
 }
