f58eb6d3de90fadb9f164647c9e259ca8fe6605f
devopscomputer <pauloti.engenharia@gmail.com>
2026-01-24 12:23:14 -0300

    chore: update components/client/booking/success/ImmediateActionButtons.tsx

--- components/client/booking/success/ImmediateActionButtons.tsx
+++ components/client/booking/success/ImmediateActionButtons.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, Platform, Dimensions } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import { AppColors, AppShadows } from '../../../../constants/appStyles';
+import { AppColors } from '../../../../constants/appStyles';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 
