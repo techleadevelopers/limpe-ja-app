c76358b959040ed2df7d785030a52f760b1d1b4f
devopscomputer <pauloti.engenharia@gmail.com>
2026-01-24 12:25:37 -0300

    chore: update temp_sch.py

--- temp_sch.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-text = Path('app/client/bookings/schedule-service.tsx').read_text().splitlines()
-for index,line in enumerate(text, start=1):
-    if 1550 <= index <= 1665:
-        print(f"{index}: {line}")
