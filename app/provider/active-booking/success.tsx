e4f5c02783815e27757690b6f37a52b850d06d8b
devopscomputer <pauloti.engenharia@gmail.com>
2026-01-24 12:16:58 -0300

    chore: update app/provider/active-booking/success.tsx

--- app/provider/active-booking/success.tsx
+++ app/provider/active-booking/success.tsx
@@ -1,4 +1,4 @@
-﻿import { Ionicons } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 import { Stack, useLocalSearchParams, useRouter } from 'expo-router';
 import { useEffect, useRef, useState } from 'react';
 import { ActivityIndicator, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
