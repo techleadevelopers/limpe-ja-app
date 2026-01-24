156de6233de21db666ea373087f39cb79889fbdb
devopscomputer <pauloti.engenharia@gmail.com>
2026-01-24 12:16:52 -0300

    chore: update app/client/support/tutorials.tsx

--- app/client/support/tutorials.tsx
+++ app/client/support/tutorials.tsx
@@ -14,7 +14,7 @@ import {
     View,
 } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { AppColors, AppShadows } from '../../../constants/appStyles';
+import { AppColors } from '../../../constants/appStyles';
 
 type TutorialCard = {
   id: string;
