1aab12c871236fafd8c91e25507eb52926f120e2
devopscomputer <pauloti.engenharia@gmail.com>
2026-01-24 12:24:04 -0300

    chore: update components/common/Header.tsx

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,7 +1,7 @@
 // src/components/Header.tsx
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, ViewStyle } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import { MaterialIcons as Icon } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import { colors } from './theme/colors';
 import { typography } from './theme/typography';
